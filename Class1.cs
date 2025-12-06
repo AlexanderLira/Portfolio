a99638891785da70a004d9018bd87c53b04d31fa
Osman Alexander Lira Caceres <liracaceres12345@outlook.es>
2025-12-06 16:14:28 +0100

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_y_objetos5
+{
+    internal static class Operaciones
+    {
+        public static double Suma(double v1, double v2)
+        {
+            return v1 + v2;
+        
+        }
+        public static double Resta(double v1, double v2)
+        {
+            return v1 - v2;
+        }
+        public static double Multiplicación(double v1, double v2)
+        {
+            return v1 * v2;
+        
+        }
+        public static double División(double v1, double v2)
+        {
+            return v1 / v2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Clases_y_objetos5;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double v1, v2 ;
+        double resultado = 0;
+        int elección = 0;
+        Console.WriteLine("1. Suma de valores.");
+        Console.WriteLine("2. Resta de valores.");
+        Console.WriteLine("3. Multiplicación de valores.");
+        Console.WriteLine("4. División de valores.");
+        Console.Write("Elija su opción: ");
+        elección=int.Parse(Console.ReadLine());
+        Console.WriteLine("primer numero");
+        v1=double.Parse(Console.ReadLine());
+        Console.WriteLine("Segundo numero");
+        v2 = double.Parse(Console.ReadLine());
+        //completar el código……
+        if(elección==1)
+        {
+            resultado = Operaciones.Suma(v1, v2);
+            Console.WriteLine("La respuesta es:\n" + resultado);
+        }
+        else
+        {
+            if(elección==2)
+            {
+                resultado = Operaciones.Resta(v1, v2);
+                Console.WriteLine("La respuesta es:\n" + resultado);
+            }
+            else
+            {
+                if(elección== 3)
+                {
+                    resultado = Operaciones.Multiplicación(v1, v2);
+                    Console.WriteLine("La respuesta es:\n" + resultado);
+                }
+                else
+                {
+                    if(elección==4)
+                    {
+                        resultado = Operaciones.División(v1, v2);
+                        Console.WriteLine("La respuesta es:\n" + resultado);
+                    }
+                }
+            }
+        }
+        Console.ReadKey();
+    }
+}
